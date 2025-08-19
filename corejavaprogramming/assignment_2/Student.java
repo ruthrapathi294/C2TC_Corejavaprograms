f72af1514ff4a81161c3fc67cba02c8879ae4e74
RUTHRAPATHI <RUTHRAPATHI@RUTHRAPATHI>
2025-08-19 11:33:59 +0530

    assignment2

---
+++ corejavaprogramming/assignment_2/Student.java
@@ -0,0 +1,14 @@
+package com.Ruthra.Assigment2;
+
+public class Student {
+    
+    // Default Constructor
+    Student() {
+        System.out.println("Student object is created");
+    }
+
+    public static void main(String[] args) {
+        // When object is created, constructor runs automatically
+        Student s1 = new Student();
+    }
+}
