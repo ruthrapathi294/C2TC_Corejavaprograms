d1ae4810907a3100a40b7be46aa101973f278882
RUTHRAPATHI <RUTHRAPATHI@RUTHRAPATHI>
2025-08-20 14:35:02 +0530

    this is day5 singleinheritance

---
+++ corejavaprogramming/day5/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,11 @@
+package singleinheritance;
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		
+
+		Student student = new Student("ruthra", "7239393949000", "Puducherry", 9888878787L, 001, "Manakula Vinayagar Institute of Technology");
+		System.out.println(student);
+	}
+
+}
